79b0699e626cd7c615cd3b879cfc4fd219da42de
Carlos Figuera La Riva <101476000@georgebrown.ca>
2026-02-22 12:40:19 -0500

    Lab05 - Add GraphQL schema with Movie type, Queries and Mutations

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,50 @@
 import { gql } from 'graphql-tag';
+
+// GraphQL Schema - Type Definitions for Movie
+const movieSchema = gql`
+  type Movie {
+      id: ID!
+          name: String!
+              director_name: String!
+                  production_house: String!
+                      release_date: String!
+                          rating: Float!
+                            }
+
+                              type Query {
+                                  # Get all movies
+                                      getAllMovies: [Movie!]!
+
+                                          # Get a single movie by ID
+                                              getMovieById(id: ID!): Movie
+
+                                                  # Get movies by director name using static methods
+                                                      getMoviesByDirector(director_name: String!): [Movie!]!
+                                                        }
+
+                                                          type Mutation {
+                                                              # Insert a new movie
+                                                                  addMovie(
+                                                                        name: String!
+                                                                              director_name: String!
+                                                                                    production_house: String!
+                                                                                          release_date: String!
+                                                                                                rating: Float!
+                                                                                                    ): Movie!
+                                                                                                    
+                                                                                                        # Update an existing movie by ID
+                                                                                                            updateMovie(
+                                                                                                                  id: ID!
+                                                                                                                        name: String
+                                                                                                                              director_name: String
+                                                                                                                                    production_house: String
+                                                                                                                                          release_date: String
+                                                                                                                                                rating: Float
+                                                                                                                                                    ): Movie!
+                                                                                                                                                    
+                                                                                                                                                        # Delete a movie by ID
+                                                                                                                                                            deleteMovie(id: ID!): Movie!
+                                                                                                                                                              }
+                                                                                                                                                              `;
+
+export default movieSchema;
